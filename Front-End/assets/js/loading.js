29a4de4ea92be56d39470ae0f5cde4cd447e2ec3
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2018-09-19 08:23:12 +0700

    REAL-6 loading animations and ajax
    try to implement ajax

--- Front-End/assets/js/loading.js
+++ Front-End/assets/js/loading.js
@@ -1,4 +1,6 @@
-$({property: 0}).animate({property: 100}, {
+$(document).on("click",".nav-link", function () {
+		var clickedBtnID = $(this).attr('id'); // or var clickedBtnID = this.id
+    $({property: 0}).animate({property: 100}, {
         duration: 1000,
         step: function() {
           var _percent = Math.round(this.property);
@@ -6,12 +8,8 @@ $({property: 0}).animate({property: 100}, {
         },
         complete: function() {
             $("#progress").addClass("done");
-            $("#page-row").toggleClass("hide");
+            $("html").load(clickedBtnID+".html");
         }
       });
-
-$(document).on("click",".nav-link", function () {
-		var clickedBtnID = $(this).attr('id'); // or var clickedBtnID = this.id
-		window.location.replace(clickedBtnID+".html");
 });
 
